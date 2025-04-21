dfd537c358b535d389257da0a6f0305b085227d6
WheelerShigley <115506442+WheelerShigley@users.noreply.github.com>
2025-04-21 11:14:00 -0500

    Cleanup
    - Removed unused/unmodified classes
    - Added credit to original author
    - Added assets folder for project

--- src/main/java/la1m1e/wanderingtrader/FrequentWanderingTrader.java
+++ src/main/java/la1m1e/wanderingtrader/FrequentWanderingTrader.java
@@ -6,12 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FrequentWanderingTrader implements ModInitializer {
-	public static final String MOD_ID = "frequentwanderingtrader";
+    public static final String MOD_ID = "frequentwanderingtrader";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-
-
-	}
+    @Override public void onInitialize() {}
 }
--- src/main/java/la1m1e/wanderingtrader/FrequentWanderingTraderServer.java
+++
@@ -1,9 +0,0 @@
-package la1m1e.wanderingtrader;
-import net.fabricmc.api.DedicatedServerModInitializer;
-import net.minecraft.world.WanderingTraderManager;
-
-public class FrequentWanderingTraderServer implements DedicatedServerModInitializer {
-    @Override
-    public void onInitializeServer() {
-   }
-}
